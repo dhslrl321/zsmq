3a6893244a65f7be610a494b1f60a2d733322222
dhslrl321 <dhslrl321@gmail.com>
2022-08-31 10:19:25 +0900

    commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # esmq
-Extremely Simple Message Queue for PoC
+
+This is an **extremely simple message queue (ESMQ)** based on a SpringBoot that can be used in various places. (study, poc ...)
+
+# Quick Start
+
+```shell
+docker run -d -p [port]:8080 dhslrl321/esmq
+```
---
+++ src/main/java/com/wonit/esmq/EsmqApplication.java
@@ -0,0 +1,13 @@
+package com.wonit.esmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsmqApplication.class, args);
+    }
+
+}
