c57c88f0818d0dfa34d94e4e7b535cd7b7eb0b69
dhslrl321 <dhslrl321@gmail.com>
2022-10-01 00:52:19 +0900

    refactor controller

--- zola-messaging-server/src/main/java/com/github/dhslrl321/zsmq/controller/CommandQueueController.java
+++ zola-messaging-server/src/main/java/com/github/dhslrl321/zsmq/controller/CommandQueueController.java
@@ -14,7 +14,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,7 +24,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/api")
 @RequiredArgsConstructor
-@CrossOrigin(value = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PATCH, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS})
+@CrossOrigin(value = "*", methods = {
+        RequestMethod.GET,
+        RequestMethod.POST,
+        RequestMethod.PATCH,
+        RequestMethod.PUT,
+        RequestMethod.DELETE,
+        RequestMethod.OPTIONS})
 public class CommandQueueController {
 
     private final ZolaQueueContainer container;
@@ -33,41 +38,60 @@ public class CommandQueueController {
     @PostMapping("/queues")
     public ResponseEntity<SimpleResponse> createQ(@RequestBody Map<String, String> requestBody) {
         String name = requestBody.get("name");
-        if (StringUtils.isBlank(name) || name.length() > 40) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        if (isBlankOrLengthOver40(name)) {
+            return badRequest();
         }
         container.register(getNewZolaQueue(name));
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(new SimpleResponse("create success"));
+        return created("create success");
     }
 
     @DeleteMapping("/queues/{name}")
     public ResponseEntity<SimpleResponse> deleteQ(@PathVariable String name) {
-        if (StringUtils.isBlank(name) || name.length() > 40) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        if (isBlankOrLengthOver40(name)) {
+            return badRequest();
         }
         if (!container.contains(name)) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            return notFound();
         }
         container.removeBy(name);
-
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(new SimpleResponse("remove success"));
+        return noContent("remove success");
     }
 
     @PostMapping("/messages")
     public ResponseEntity<SimpleResponse> push(@RequestBody MessageModel request) {
         ZolaMessage zolaMessage = ModelConverter.convert(request);
         container.pushBy(zolaMessage);
-        return ResponseEntity.status(HttpStatus.CREATED).body(new SimpleResponse("message add success"));
+        return created("message add success");
     }
 
     @DeleteMapping("/queues/{name}/acknowledge")
     public ResponseEntity<SimpleResponse> ack(@PathVariable String name) {
         container.popBy(QueueName.of(name));
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(new SimpleResponse("ack success"));
+        return noContent("ack success");
     }
 
     private ZolaSimpleQueue getNewZolaQueue(String name) {
         return ZolaSimpleQueue.newInstance(QueueName.of(name));
+    }
+
+    private boolean isBlankOrLengthOver40(String name) {
+        return StringUtils.isBlank(name) || name.length() > 40;
+    }
+
+    private ResponseEntity<SimpleResponse> badRequest() {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    private ResponseEntity<SimpleResponse> created(String message) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(new SimpleResponse(message));
+    }
+
+    private ResponseEntity<SimpleResponse> notFound() {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    private ResponseEntity<SimpleResponse> noContent(String message) {
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(new SimpleResponse(message));
     }
 }
