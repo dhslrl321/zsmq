2b79f0c83301c66bb0deaa77e9445a72e5c67661
dhslrl321 <dhslrl321@gmail.com>
2022-10-02 12:29:12 +0900

    fix sleep

--- zola-messaging-core/src/main/java/com/github/dhslrl321/zsmq/listener/task/ListeningTask.java
+++ zola-messaging-core/src/main/java/com/github/dhslrl321/zsmq/listener/task/ListeningTask.java
@@ -5,9 +5,11 @@ import com.github.dhslrl321.zsmq.listener.DeletionPolicy;
 import com.github.dhslrl321.zsmq.listener.ListeningInformation;
 import com.github.dhslrl321.zsmq.listener.MessageListener;
 import com.github.dhslrl321.zsmq.listener.strategy.ListeningStrategy;
-import java.util.Objects;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.ThreadUtils;
 
 @RequiredArgsConstructor
 public class ListeningTask implements Runnable {
@@ -20,11 +22,21 @@ public class ListeningTask implements Runnable {
 
     @Override
     public void run() {
-        while(true) {
-            ZolaMessage message = strategy.peek(listeningInfo);
-            listener.listen(message.getPayload().getValue());
-            handleAcknowledgement();
+        boolean loop = true;
+        while(loop) {
+            try {
+                ZolaMessage message = strategy.peek(listeningInfo);
+                listener.listen(message.getPayload().getValue());
+                handleAcknowledgement();
+                sleep();
+            } catch (Exception e) {
+                loop = false;
+            }
         }
+    }
+
+    private void sleep() throws InterruptedException {
+        ThreadUtils.sleep(Duration.of(1, ChronoUnit.SECONDS));
     }
 
     private void handleAcknowledgement() {
