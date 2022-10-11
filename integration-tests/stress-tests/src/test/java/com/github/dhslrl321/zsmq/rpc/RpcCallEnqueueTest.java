1b37b371f2def602832a6efa8f57c6a982dce97a
dhslrl321 <dhslrl321@gmail.com>
2022-10-11 10:37:39 +0900

    add test block

--- integration-tests/stress-tests/src/test/java/com/github/dhslrl321/zsmq/rpc/RpcCallEnqueueTest.java
+++ integration-tests/stress-tests/src/test/java/com/github/dhslrl321/zsmq/rpc/RpcCallEnqueueTest.java
@@ -1,17 +1,13 @@
-package com.github.dhslrl321.zsmq;
+package com.github.dhslrl321.zsmq.rpc;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
 import com.github.dhslrl321.zsmq.client.ZolaClientConfig;
 import com.github.dhslrl321.zsmq.client.ZolaQueueMessageTemplate;
 import com.github.dhslrl321.zsmq.http.ZolaHttpClient;
-import java.util.function.IntConsumer;
 import java.util.stream.IntStream;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -19,33 +15,30 @@ import org.junit.jupiter.api.Test;
 public class RpcCallEnqueueTest {
 
     private static final int SIZE = 1_000_000;
-    public static final String TOOK_MS_MESSAGE = "test took (ms) : ";
+
 
     ZolaQueueMessageTemplate sut;
 
-
-    long before;
-    long after;
+    long start;
+    long end;
 
     @BeforeEach
     void setUp() {
         sut = new ZolaQueueMessageTemplate(
                 new ZolaClientConfig("http://localhost:8291"), new ZolaHttpClient());
 
-        before = System.currentTimeMillis();
+        start = System.currentTimeMillis();
     }
 
     @AfterEach
     void tearDown() {
-        after = System.currentTimeMillis();
-        long diff = (after - before);
-        System.out.println(TOOK_MS_MESSAGE + diff + " (ms)");
+        end = System.currentTimeMillis();
+        ResultReporter.reportToConsole(end, start);
     }
-
 
     @Test
     @DisplayName("rpc call 1,000,000 messages")
-    void name() {
+    void TEST() {
         IntStream.range(0, SIZE)
                 .forEach((i) -> sut.convertAndSend("TESTING-QUEUE", "dummy message, " + i));
     }
